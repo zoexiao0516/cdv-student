bd3b1095fb689597833615824631469319e9c623
Zoe Xiao <yx1750@nyu.edu>
2021-02-01 16:53:46 +0800

    Creating a new folder

---
+++ coding-exercises/coding-exercise-2/README.md
@@ -0,0 +1,10 @@
+# 👾 CDV Coding Exercise 1 ~ Foundation
+
+#### See my work [here](https://zoexiao0516.github.io/cdv-student/coding-exercises/coding-exercise-2/index.html)
+
+#### Difficulty:
+
+#### Keywords:
+
+#### Strategy:
+1. ...
---
+++ coding-exercises/coding-exercise-2/data.js
@@ -0,0 +1,92 @@
+[
+    {
+        "timestamp": "2021-01-28T03:34:37.039Z",
+        "loyaltyAndUnderstanding": 5,
+        "anExcitingPersonality": 5,
+        "emotionalStability": 5,
+        "similarities": 1,
+        "physicalAttractiveness": 3,
+        "incomeearningPotential": 2
+    },
+    {
+        "timestamp": "2021-01-28T03:39:14.187Z",
+        "loyaltyAndUnderstanding": 5,
+        "anExcitingPersonality": 4,
+        "emotionalStability": 4,
+        "similarities": 2,
+        "physicalAttractiveness": 5,
+        "incomeearningPotential": 5
+    },
+    {
+        "timestamp": "2021-01-28T04:02:34.369Z",
+        "loyaltyAndUnderstanding": 5,
+        "anExcitingPersonality": 4,
+        "emotionalStability": 5,
+        "similarities": 3,
+        "physicalAttractiveness": 3,
+        "incomeearningPotential": 3
+    },
+    {
+        "timestamp": "2021-01-28T04:05:17.319Z",
+        "loyaltyAndUnderstanding": 4,
+        "anExcitingPersonality": 3,
+        "emotionalStability": 4,
+        "similarities": 2,
+        "physicalAttractiveness": 3,
+        "incomeearningPotential": 3
+    },
+    {
+        "timestamp": "2021-01-28T04:32:47.816Z",
+        "loyaltyAndUnderstanding": 5,
+        "anExcitingPersonality": 4,
+        "emotionalStability": 4,
+        "similarities": 3,
+        "physicalAttractiveness": 3,
+        "incomeearningPotential": 4
+    },
+    {
+        "timestamp": "2021-01-28T07:32:50.132Z",
+        "loyaltyAndUnderstanding": 5,
+        "anExcitingPersonality": 5,
+        "emotionalStability": 4,
+        "similarities": 5,
+        "physicalAttractiveness": 4,
+        "incomeearningPotential": 3
+    },
+    {
+        "timestamp": "2021-01-28T10:21:48.431Z",
+        "loyaltyAndUnderstanding": 5,
+        "anExcitingPersonality": 5,
+        "emotionalStability": 4,
+        "similarities": 3,
+        "physicalAttractiveness": 4,
+        "incomeearningPotential": 4
+    },
+    {
+        "timestamp": "2021-01-29T03:22:18.935Z",
+        "loyaltyAndUnderstanding": 5,
+        "anExcitingPersonality": 5,
+        "emotionalStability": 4,
+        "similarities": 4,
+        "physicalAttractiveness": 4,
+        "incomeearningPotential": 4
+    },
+    {
+        "timestamp": "2021-01-29T03:30:15.565Z",
+        "loyaltyAndUnderstanding": 3,
+        "anExcitingPersonality": 4,
+        "emotionalStability": 4,
+        "similarities": 4,
+        "physicalAttractiveness": 4,
+        "incomeearningPotential": 2
+    },
+    {
+        "timestamp": "2021-01-31T14:21:07.489Z",
+        "loyaltyAndUnderstanding": 5,
+        "anExcitingPersonality": 4,
+        "emotionalStability": 4,
+        "similarities": 3,
+        "physicalAttractiveness": 3,
+        "incomeearningPotential": 5
+    }
+]
