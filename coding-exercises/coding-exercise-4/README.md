6acccd73588bdfc0b8b8f62c488dab4834729da1
Zoe Xiao <yx1750@nyu.edu>
2021-02-23 22:16:54 +0800

    my Atom pushing is weired

---
+++ coding-exercises/coding-exercise-4/README.md
@@ -0,0 +1,12 @@
+# 💎 Coding Exercise 4 (Grouping Elements)
+
+## create groups for each data point and assign attributes 🟤
+
+#### See my work [here](https://zoexiao0516.github.io/cdv-student/coding-exercises/coding-exercise-4/index.html)
+
+#### Inspirations
+Shirley Wu's personal website [here](https://shirleywu.studio/)
+
+#### Difficulty
+1. Assign attributes of each data point to the data group it belongs
+1. Rotate the SVG path around its pivot
--- coding-exercises/coding-exercise-4/script.js
+++ coding-exercises/coding-exercise-4/script.js
@@ -1,88 +1,74 @@
+groupContSize = 100;
+
 let viz = d3.select("#viz-container")
   .append("svg")
     .attr("id", "viz")
-    .attr("width", 600)
-    .attr("height", 400)
-    .attr("background-color", "lavendar")
+    .attr("width", groupContSize * 16)
+    .attr("height", groupContSize * 10)
+    // .attr("background-color", "lavendar")
 ;
 
-function randomX(){
-  return Math.random() * 600;
-}
+// myPath = ['M 0,0 L 10,-15 L -8,-40 L 0,-50 L 8,-40 L -10,-15 L 0,0'];
+myPath = ['M 50,50 L 60,35 L 42,10 L 50,0 L 58,10 L 40,35 L 50,50'];
 
-function randomY(){
-  return Math.random() * 400;
-}
+// myPath.attr("transform", "translate(50, 50)");
 
-function randomTranslate(){
-  let x = Math.random() * 600;
-  let y = Math.random() * 400;
-  return "translate(" + x + "," + y + ")";
-}
+let data = d3.json("data.json").then(gotData);
+
+var dataPointLength;
 
 function gotData(incomingData){
   console.log("the incoming data is", incomingData);
+  // var dataPointLength = incomingData.length;
+  // console.log("the length of incoming data is", dataPointLength);
 
-  // viz.selectAll("circle").data(incomingData).enter()
-  //   .append("circle")
-  //     .attr("cx", randomX)
-  //     .attr("cy", randomY)
-  //     .attr("r", 20)
-  //     .attr("fill", "white")
-  //     .attr("class", "class1")
-  // ;
-  //
-  // viz.selectAll(".class2").data(incomingData).enter()  // nothing is entered here
-  //   .append("circle")
-  //     .attr("cx", randomX)
-  //     .attr("cy", randomY)
-  //     .attr("r", 20)
-  //     .attr("fill", "pink")
-  //     .attr("class", "class2")
-  // ;
-
-  // viz.selectAll(".food").data(incomingData).enter()
-  //   .append("rect")
-  //     .attr("x", randomX)
-  //     .attr("y", randomY)
-  //     .attr("width", 20)
-  //     .attr("height", 20)
-  //     .attr("class", "food")
-  //
-  // ;
-  //
-  // viz.selectAll(".foodtext").data(incomingData).enter()
-  //   .append("text")
-  //     .attr("x", randomX)
-  //     .attr("y", randomY)
-  //     .text("food")
-  //     .attr("fill", "blue")
-  // ;
-
-  let datagroups = viz.selectAll(".datagroup").data(incomingData).enter()
-    .append("g")  // the selection is all groups now
-      .attr("class", "datagroup")
+  let groups = viz.selectAll(".group").data(incomingData).enter()
+    .append("g")
+      .attr("class", "group")
+      .attr('transform', createGroup)
   ;
 
-  datagroups.append("circle") // apply to each one in the datagroups
-    .attr("cx", 0)
-    .attr("cy", 0)
-    .attr("r", 20)
-  ;
+  groups.append("circle")
+    .attr("cx", groupContSize / 2)
+    .attr("cy", groupContSize / 2)
+    .attr("r", groupContSize / 2)
+    .attr("fill", "none")
+    .attr("stroke", "gray")
+    .attr("stroke-width", 3)
 
-  datagroups.append("text")
-    .attr("x", 20)
-    .attr("y", 10)
-    .text("Hello")
-  ;
+  groups.append("circle")
+    .attr("cx", groupContSize / 2)
+    .attr("cy", groupContSize / 2)
+    .attr("r", 2)
+    .attr("fill", "gray")
 
-  datagroups.attr("transform", randomTranslate);
+  groups.append("path")
+    .attr("d", myPath)
+    .attr('transform', pathRotate)
+    .attr("fill", chooseColor)
+  ;
 }
 
-d3.json("data.json").then(gotData);
+var i = 0;
 
-// why groups?
-  // clean structure
-  // complex shapes / more expressive
-  // move multiple shapes at the same time
-  // link text to your shapes
+function createGroup(datapoint) {
+  let x = (i % 8 * groupContSize * 2 + 100);
+  let y = (Math.floor(i / 8) * groupContSize * 2 + 100);
+  console.log(i, x, y);
+  i += 1;
+  let groupSize = datapoint.diameter * 0.1;
+  return "translate(" + x + "," + y + ")scale(" + groupSize + ")";
+}
+
+function pathRotate(datapoint){
+  let howLong = datapoint.howLongHaveIUsedIt;
+  let angle = howLong * 36
+  console.log("how long: ", howLong, "angle:", angle);
+  return "rotate(" + angle + " " + groupContSize / 2 + " " + groupContSize / 2 + ")";
+}
+
+function chooseColor(datapoint){
+  let howLong = datapoint.howLongHaveIUsedIt;
+  let angle = howLong * 36
+  return d3.interpolateWarm(angle / 360);
+}
